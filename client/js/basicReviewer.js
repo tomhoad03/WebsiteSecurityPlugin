c24ccb1da473e2dddd30f73531be3c099795b995
Tom Hoad <40165392+tomhoad03@users.noreply.github.com>
2021-12-11 19:51:31 +0000

    Website file caching

--- client/js/basicReviewer.js
+++ client/js/basicReviewer.js
@@ -1,6 +1,6 @@
 // Runs when the popup loads
 window.addEventListener("DOMContentLoaded", async() => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
 
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
@@ -10,26 +10,26 @@ window.addEventListener("DOMContentLoaded", async() => {
 
 // Performs a basic code review of the websites
 function basicReview() {
-    let href = window.location.href; // gets full href
-    let domain = window.location.hostname; // host domain (local host for testbed)
-    let path = window.location.pathname; // path and filename of the current page
-    let protocol = window.location.protocol; // http or https
-
     let ws = new WebSocket("ws://localhost:8080");
 
     ws.addEventListener("open", wss => {
         console.log("We are connected.");
 
         ws.send(JSON.stringify({
+            id: "window",
             href: window.location.href,
             domain: window.location.hostname,
             path: window.location.pathname,
             protocol: window.location.protocol
         }));
-
     });
 
     ws.addEventListener("message", message => {
-        console.log(message.data);
+        const data = JSON.parse(message.data);
+        console.log(data);
+
+        if (data.id === "score") {
+            console.log("Score = " + data.score);
+        }
     });
 }
---
+++ server/server.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({port: 8080});
+
+// Start a connection with the plugin
+wss.on("connection", ws => {
+    console.log("Client connected.")
+
+    // Listen out for information from the plugin
+    ws.on("message", message => {
+        const data = JSON.parse(message);
+        console.log(data);
+        let score = 0;
+
+        // Compute the information from the plugin
+        if (data.id === "window") {
+            if (data.protocol === "https:") {
+                score = score + 1;
+            }
+        }
+
+        // Store a list of secure and insecure websites
+        const httpsData = fs.readFileSync(process.cwd() + "/cache/websites/https.txt", "utf8");
+        const newHttpsData = httpsData.split("\n").filter(line => line !== data.domain).join("\n");
+        fs.writeFileSync(process.cwd() + "/cache/websites/https.txt", newHttpsData);
+
+        const httpData = fs.readFileSync(process.cwd() + "/cache/websites/http.txt", "utf8");
+        const newHttpData = httpData.split("\n").filter(line => line !== data.domain).join("\n");
+        fs.writeFileSync(process.cwd() + "/cache/websites/http.txt", newHttpData);
+
+        if (score > 0) {
+            fs.appendFileSync(process.cwd() + "/cache/websites/https.txt", data.domain + "\n", () => {
+                console.log(data.domain + " > /cache/websites/https.txt");
+            });
+        } else {
+            fs.appendFileSync(process.cwd() + "/cache/websites/http.txt", data.domain + "\n", () => {
+                console.log(data.domain + " > /cache/websites/http.txt");
+            });
+        }
+
+        // Update the plugin with the current security rating
+        ws.send(JSON.stringify({
+            id: "score",
+            score: score
+        }));
+    })
+});
