87089e9e7b241039b2121be128f17fee1d4cb426
Tom <tom.hoad03@gmail.com>
2022-01-02 16:15:38 +0000

    Testing script integrity

--- server/server.js
+++ server/server.js
@@ -14,31 +14,41 @@ wss.on("connection", ws => {
         console.log(serverMsg);
 
         let score = 0;
-        let httpsProtocols, scriptIntegrity;
+        let httpsProtocols = false, scriptIntegrity = false, clientSideComments = false;
 
         // Compute the information from the plugin
         if (data.id === "window") {
+
+            // Check if the webpage uses https protocols
             if (data.protocol === "https:") {
                 httpsProtocols = true;
                 score++;
             }
-            if (checkIntegrity(data.html)) {
+
+            // Check if external scripts are secure
+            if (checkScriptIntegrity(data.html)) {
                 scriptIntegrity = true;
                 score++;
-
-                /*
-                Validates HTML files for compliance against the W3C standards and performs linting to assess code quality against best practices.
-                Find missing or unbalanced HTML tags in your documents, stray characters, duplicate IDs, missing or invalid attributes and other recommendations.
-                Supports HTML5, SVG 1.1, MathML 3.0, ITS 2.0, RDFa Lite 1.1. Implementation is based on Validator.Nu.
-                 */
             }
+
+            // Check if any comments are left in the html
+            if (checkClientSideComments(data.html)) {
+                clientSideComments = true;
+                score++;
+            }
+            /*
+            Validates HTML files for compliance against the W3C standards and performs linting to assess code quality against best practices.
+            Find missing or unbalanced HTML tags in your documents, stray characters, duplicate IDs, missing or invalid attributes and other recommendations.
+            Supports HTML5, SVG 1.1, MathML 3.0, ITS 2.0, RDFa Lite 1.1. Implementation is based on Validator.Nu.
+             */
         }
 
         const contents = "Domain: " + data.domain + "\n"
                          + "Score: " + score + "\n\n"
                          + "Vulnerabilities\n\n"
                          + "HTTPS Protocols: " + httpsProtocols + "\n"
-                         + "Script Integrity: " + scriptIntegrity + "\n";
+                         + "Script Integrity: " + scriptIntegrity + "\n"
+                         + "Client Side Comments: " + clientSideComments + "\n";
 
         fs.writeFileSync(process.cwd() + "\\server\\cache\\websites\\" + data.domain + ".txt", contents);
 
@@ -51,18 +61,39 @@ wss.on("connection", ws => {
 });
 
 // CWE-353
-function checkIntegrity(html) {
-    if (html.indexOf("<script") === -1) {
+// https://rules.sonarsource.com/html/tag/cwe/RSPEC-5725
+// https://cwe.mitre.org/data/definitions/353.html
+function checkScriptIntegrity(html) {
+    if (!html.includes("<script")) {
         return true;
     } else {
+        // Get the src and any internal script
         let scriptTag = html.substring(html.indexOf("<script"), html.indexOf(">", html.indexOf("<script")) + 1);
-        // let scriptInTag = html.substring(html.indexOf(">", html.indexOf("<script")) + 1, html.indexOf("</script>", html.indexOf("<script")));
+        let scriptInTag = html.substring(html.indexOf(">", html.indexOf("<script")) + 1, html.indexOf("</script>", html.indexOf("<script")));
+        let src = "", integrity = "";
 
-        if (scriptTag.indexOf("src=") !== -1) {
-            let src = scriptTag.substring(scriptTag.indexOf("src=") + 5, scriptTag.indexOf("\"", scriptTag.indexOf("src=") + 5))
-            console.log(scriptTag + "\n" + src + "\n");
+        // Get src and integrity values
+        if (scriptTag.includes("src=")) {
+            src = scriptTag.substring(scriptTag.indexOf("src=") + 5, scriptTag.indexOf("\"", scriptTag.indexOf("src=") + 5))
+        }
+        if (scriptTag.includes("integrity=")) {
+            integrity = scriptTag.substring(scriptTag.indexOf("integrity=") + 11, scriptTag.indexOf("\"", scriptTag.indexOf("integrity=") + 11))
         }
 
-        return checkIntegrity(html.substring(html.indexOf("</script>") + 9));
+        // Return true if script is internal or is external with integrity
+        if ((scriptTag.includes("crossorigin=\"anonymous\"") && integrity.length > 0) || !src.includes("http")) {
+            return checkScriptIntegrity(html.substring(html.indexOf("</script>") + 9));
+        } else {
+            return false;
+        }
+    }
+}
+
+// CWE-615
+// https://rules.sonarsource.com/html/tag/cwe/RSPEC-1876
+// https://cwe.mitre.org/data/definitions/615.html
+function checkClientSideComments(html) {
+    if (!html.includes("<!--") && !html.includes("-->")) {
+        return true;
     }
 }
