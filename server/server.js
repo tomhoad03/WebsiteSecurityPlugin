0a45e650e31df2eabfc1818fc3b9bc2742558d2d
tomho <tom.hoad03@gmail.com>
2022-02-08 17:38:31 +0000

    Error catching

--- server/server.js
+++ server/server.js
@@ -26,134 +26,136 @@ wss.on("connection", ws => {
                 score++;
             }
 
-            // Check if external and internal scripts are used and secure if necessary
-            scriptIntegrityTest = checkScriptIntegrityTest(data.html);
-            internalScriptTest = checkInternalScriptTest(data.html);
+            try {
+                // Check if external and internal scripts are used and secure if necessary
+                scriptIntegrityTest = checkScriptIntegrityTest(data.html);
+                internalScriptTest = checkInternalScriptTest(data.html);
 
-            // CWE-353
-            // https://rules.sonarsource.com/html/tag/cwe/RSPEC-5725
-            // https://cwe.mitre.org/data/definitions/353.html
-            function checkScriptIntegrityTest(html) {
-                if (!html.includes("<script")) {
-                    score++;
-                    return true;
-                } else {
-                    let scriptTag = html.substring(html.indexOf("<script"), html.indexOf(">", html.indexOf("<script")) + 1);
-                    let src = "", integrity = "", nonce = "";
-
-                    // Get src and integrity values
-                    if (scriptTag.includes("src=")) {
-                        src = scriptTag.substring(scriptTag.indexOf("src=") + 5, scriptTag.indexOf("\"", scriptTag.indexOf("src=") + 5))
-                    }
-                    if (scriptTag.includes("integrity=")) {
-                        integrity = scriptTag.substring(scriptTag.indexOf("integrity=") + 11, scriptTag.indexOf("\"", scriptTag.indexOf("integrity=") + 11))
-                    }
-                    if (scriptTag.includes("nonce=")) {
-                        nonce = scriptTag.substring(scriptTag.indexOf("nonce=") + 7, scriptTag.indexOf("\"", scriptTag.indexOf("nonce=") + 7))
-                    }
-
-                    // Return true if script is internal or is external with integrity
-                    if ((scriptTag.includes("crossorigin=\"anonymous\"") && integrity.length > 0) || nonce.length > 0 || !src.includes("http")) {
-                        return checkScriptIntegrityTest(html.substring(html.indexOf("</script>") + 9));
+                // CWE-353
+                // https://rules.sonarsource.com/html/tag/cwe/RSPEC-5725
+                // https://cwe.mitre.org/data/definitions/353.html
+                function checkScriptIntegrityTest(html) {
+                    if (!html.includes("<script")) {
+                        score++;
+                        return true;
                     } else {
-                        return false;
+                        let scriptTag = html.substring(html.indexOf("<script"), html.indexOf(">", html.indexOf("<script")) + 1);
+                        let src = "", integrity = "", nonce = "";
+
+                        // Get src and integrity values
+                        if (scriptTag.includes("src=")) {
+                            src = scriptTag.substring(scriptTag.indexOf("src=") + 5, scriptTag.indexOf("\"", scriptTag.indexOf("src=") + 5))
+                        }
+                        if (scriptTag.includes("integrity=")) {
+                            integrity = scriptTag.substring(scriptTag.indexOf("integrity=") + 11, scriptTag.indexOf("\"", scriptTag.indexOf("integrity=") + 11))
+                        }
+                        if (scriptTag.includes("nonce=")) {
+                            nonce = scriptTag.substring(scriptTag.indexOf("nonce=") + 7, scriptTag.indexOf("\"", scriptTag.indexOf("nonce=") + 7))
+                        }
+
+                        // Return true if script is internal or is external with integrity
+                        if ((scriptTag.includes("crossorigin=\"anonymous\"") && integrity.length > 0) || nonce.length > 0 || !src.includes("http")) {
+                            return checkScriptIntegrityTest(html.substring(html.indexOf("</script>") + 9));
+                        } else {
+                            return false;
+                        }
                     }
                 }
-            }
 
-            function checkInternalScriptTest(html) {
-                if (!html.includes("<script")) {
-                    score++;
-                    return true;
-                } else {
-                    let scriptInTag = html.substring(html.indexOf(">", html.indexOf("<script")) + 1, html.indexOf("</script>", html.indexOf("<script")));
-
-                    // Return true if is external
-                    if (scriptInTag.length === 0) {
-                        return checkInternalScriptTest(html.substring(html.indexOf("</script>") + 9));
+                function checkInternalScriptTest(html) {
+                    if (!html.includes("<script")) {
+                        score++;
+                        return true;
                     } else {
-                        return false;
+                        let scriptInTag = html.substring(html.indexOf(">", html.indexOf("<script")) + 1, html.indexOf("</script>", html.indexOf("<script")));
+
+                        // Return true if is external
+                        if (scriptInTag.length === 0) {
+                            return checkInternalScriptTest(html.substring(html.indexOf("</script>") + 9));
+                        } else {
+                            return false;
+                        }
                     }
                 }
-            }
 
-            // Check if any comments are left in the html
-            if (checkClientSideCommentsTest(data.html)) {
-                clientSideCommentsTest = true;
-                score++;
-            }
-
-            // CWE-615
-            // https://rules.sonarsource.com/html/tag/cwe/RSPEC-1876
-            // https://cwe.mitre.org/data/definitions/615.html
-            function checkClientSideCommentsTest(html) {
-                return !html.includes("<!--") && !html.includes("-->");
-            }
-
-            // Check if any hyperlinks on a page are suspicious.
-            untrustedLinksTest = checkUntrustedLinksTest(data.html);
-
-            function checkUntrustedLinksTest(html) {
-                if (!html.includes("href=")) {
+                // Check if any comments are left in the html
+                if (checkClientSideCommentsTest(data.html)) {
+                    clientSideCommentsTest = true;
                     score++;
-                    return true;
-                } else {
-                    let link = html.substring(html.indexOf("href=\"") + 6, html.indexOf("\"", html.indexOf("href=\"") + 6));
+                }
 
-                    // Return true if is external
-                    if (!link.includes("http") || link.includes("https")) {
-                        // console.log(link);
-                        return checkUntrustedLinksTest(html.substring(html.indexOf("href=\"") + 6));
+                // CWE-615
+                // https://rules.sonarsource.com/html/tag/cwe/RSPEC-1876
+                // https://cwe.mitre.org/data/definitions/615.html
+                function checkClientSideCommentsTest(html) {
+                    return !html.includes("<!--") && !html.includes("-->");
+                }
+
+                // Check if any hyperlinks on a page are suspicious.
+                untrustedLinksTest = checkUntrustedLinksTest(data.html);
+
+                function checkUntrustedLinksTest(html) {
+                    if (!html.includes("href=")) {
+                        score++;
+                        return true;
                     } else {
-                        return false;
+                        let link = html.substring(html.indexOf("href=\"") + 6, html.indexOf("\"", html.indexOf("href=\"") + 6));
+
+                        // Return true if is external
+                        if (!link.includes("http") || link.includes("https")) {
+                            // console.log(link);
+                            return checkUntrustedLinksTest(html.substring(html.indexOf("href=\"") + 6));
+                        } else {
+                            return false;
+                        }
                     }
                 }
-            }
 
-            // Performs XSS vulnerability checks.
-            basicXXSTest = checkBasicXXSTest();
+                // Performs XSS vulnerability checks.
+                basicXXSTest = checkBasicXXSTest();
 
-            function checkBasicXXSTest() {
-                ws.send(JSON.stringify({
-                    id: "xss",
-                    message: "<SCRIPT SRC=http://xss.rocks/xss.js></SCRIPT>"
-                }));
-                score++;
-                return true;
-            }
+                function checkBasicXXSTest() {
+                    ws.send(JSON.stringify({
+                        id: "xss",
+                        message: "<SCRIPT SRC=http://xss.rocks/xss.js></SCRIPT>"
+                    }));
+                    score++;
+                    return true;
+                }
 
-            // Checks if information is autofilled.
-            if (data.autoFill1) {
-                score++;
-            }
-            if (data.autoFill2) {
-                score++;
-            }
+                // Checks if information is autofilled.
+                if (data.autoFill1) {
+                    score++;
+                }
+                if (data.autoFill2) {
+                    score++;
+                }
 
-            // Checks the security of the cookies
-            cookieSecurity = checkCookieSecurity(data.cookies);
-            timelyCookies = checkCookieTimeliness(data.cookies);
+                // Checks the security of the cookies
+                cookieSecurity = checkCookieSecurity(data.cookies);
+                timelyCookies = checkCookieTimeliness(data.cookies);
 
-            function checkCookieSecurity(cookies) {
-                cookies.forEach(cookie => {
-                    if (cookie.secure === false) {
-                        return false;
-                    }
-                });
-                score++;
-                return true;
-            }
+                function checkCookieSecurity(cookies) {
+                    cookies.forEach(cookie => {
+                        if (cookie.secure === false) {
+                            return false;
+                        }
+                    });
+                    score++;
+                    return true;
+                }
 
-            function checkCookieTimeliness(cookies) {
-                cookies.forEach(cookie => {
-                    if (cookie.expirationDate < Math.round(Date.now() / 1000)) {
-                        return false;
-                    }
-                });
-                console.log(score);
-                score++;
-                console.log(score);
-                return true;
+                function checkCookieTimeliness(cookies) {
+                    cookies.forEach(cookie => {
+                        if (cookie.expirationDate < Math.round(Date.now() / 1000)) {
+                            return false;
+                        }
+                    });
+                    score++;
+                    return true;
+                }
+            } catch(err) {
+                console.log(err);
             }
 
             /*
